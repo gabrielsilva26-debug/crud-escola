9d40b31140ed149066a83e3440715a03132d478b
gabrielaraujo8671-jpg <gabrielaraujo8671@gmail.com>
2025-09-28 17:13:39 -0300

    atualização

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use('/professores', professoresRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${3000}`);
 });
