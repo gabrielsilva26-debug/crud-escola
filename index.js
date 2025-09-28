50c4a416a83fce6840d78840893fda1d26e988ca
gabrielaraujo8671-jpg <gabrielaraujo8671@gmail.com>
2025-09-28 16:26:02 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// importar routers (após os arquivos serem criados)
+const alunosRouter = require('./routes/alunos');       // Membro 1
+const professoresRouter = require('./routes/professores'); // Membro 2
+
+app.use('/alunos', alunosRouter);
+app.use('/professores', professoresRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
